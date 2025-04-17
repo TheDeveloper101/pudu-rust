4052e90fb3b1d2e3f5de662d1033396214e7173c
Sai Divvela <saidivvela@sais-air-2.umd.edu>
2025-04-17 16:57:04 -0400

    Reimplemented pudu in rust using a trait based approach.

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,58 @@
-use std::collections::{HashMap, HashSet};
-use std::hash::Hash;
+pub trait State: Copy + Clone + PartialEq + Eq + std::fmt::Debug {}
 
-// Define traits that represent peripherals and their states
-pub trait Peripheral: Clone + Eq + Hash {}
-
-pub trait PeripheralState: Clone + Eq + Hash {
-    type PeripheralType: Peripheral;
-    
-    // Returns the off state for a peripheral
-    fn off() -> Self;
+pub trait Peripheral {
+    type State: State;
 }
 
-// Main typestate tracking system
-pub struct Pudu<P, S>
-where
-    P: Peripheral,
-    S: PeripheralState<PeripheralType = P>,
-{
-    // Track the current state of each peripheral
-    states: HashMap<P, S>,
-    
-    // Track functions that manipulate peripherals
-    sleep_functions: HashMap<P, Vec<fn(&P)>>,
-    restore_functions: HashMap<P, Vec<fn(&P)>>,
-    access_functions: HashMap<P, Vec<fn(&P)>>,
-    
-    // Track ISRs associated with peripherals
-    isr_mappings: HashMap<P, HashSet<String>>,
-    active_isrs: HashSet<String>,
+pub trait InState<S> {}
+
+pub trait ValidTransition<From, To> {}
+
+// a peripheral that has state
+pub struct Stateful<P, S> {
+    pub peripheral: P,
+    _state: std::marker::PhantomData<S>,
 }
 
-impl<P, S> Pudu<P, S>
-where
-    P: Peripheral,
-    S: PeripheralState<PeripheralType = P>,
+impl<P: Peripheral, S> Stateful<P, S> 
+where 
+    S: State,
+    P: InState<S>,
 {
-    pub fn new() -> Self {
-        Self {
-            states: HashMap::new(),
-            sleep_functions: HashMap::new(),
-            restore_functions: HashMap::new(),
-            access_functions: HashMap::new(),
-            isr_mappings: HashMap::new(),
-            active_isrs: HashSet::new(),
+    pub fn new<NewState: State>(peripheral: P) -> Self {
+        Stateful {
+            peripheral,
+            _state: std::marker::PhantomData,
         }
     }
-        
-    // Typestate Tracking
-    pub fn enroll(&mut self, peripheral: P) {
-        self.states.insert(peripheral.clone(), S::off());
-    }
     
-    pub fn update(&mut self, peripheral: &P, state: S) {
-        self.states.insert(peripheral.clone(), state);
-    }
-    
-    // Function Registration
-    pub fn register_sleep(&mut self, peripheral: P, func: fn(&P)) {
-        self.sleep_functions
-            .entry(peripheral)
-            .or_insert_with(Vec::new)
-            .push(func);
-    }
-    
-    pub fn register_restore(&mut self, peripheral: P, func: fn(&P)) {
-        self.restore_functions
-            .entry(peripheral)
-            .or_insert_with(Vec::new)
-            .push(func);
-    }
-    
-    pub fn register_access(&mut self, peripheral: P, func: fn(&P)) {
-        self.access_functions
-            .entry(peripheral)
-            .or_insert_with(Vec::new)
-            .push(func);
-    }
-    
-    // ISR Registration
-    pub fn enable_isr(&mut self, peripheral: &P, isr_name: &str) {
-        self.isr_mappings
-            .entry(peripheral.clone())
-            .or_insert_with(HashSet::new)
-            .insert(isr_name.to_string());
-        self.active_isrs.insert(isr_name.to_string());
-    }
-    
-    pub fn disable_isr(&mut self, _peripheral: &P, isr_name: &str) {
-        self.active_isrs.remove(isr_name);
-    }
-    
-    // Checkers
-    pub fn check(&self, peripheral: &P, expected_state: &S) -> bool {
-        if let Some(state) = self.states.get(peripheral) {
-            state == expected_state
-        } else {
-            false
+    pub fn transition<NewS: State>(self) -> Stateful<P, NewS> 
+    where 
+        P: InState<NewS>,
+        P: ValidTransition<S, NewS>,
+    {
+        Stateful {
+            peripheral: self.peripheral,
+            _state: std::marker::PhantomData,
         }
     }
+    
+    pub fn get(&self) -> &P {
+        &self.peripheral
+    }
+    
+    pub fn expect<ExpectedS: State>(self) -> Self 
+    where 
+        S: std::cmp::PartialEq<ExpectedS>,
+        P: InState<ExpectedS>
+    {
+        self
+    }
+}
+
+#[macro_export]
+macro_rules! define_transition {
+    ($peripheral:ty, $from:ty, $to:ty) => {
+        impl ValidTransition<$from, $to> for $peripheral {}
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,175 @@
+pub trait State: Copy + Clone + PartialEq + Eq + std::fmt::Debug {}
+
+pub trait Peripheral {
+    type State: State;
+}
+
+pub trait InState<S> {}
+
+pub trait ValidTransition<From, To> {}
+
+// a peripheral that has state
+pub struct Stateful<P, S> {
+    pub peripheral: P,
+    _state: std::marker::PhantomData<S>,
+}
+
+impl<P: Peripheral, S> Stateful<P, S> 
+where 
+    S: State,
+    P: InState<S>,
+{
+    pub fn new<NewState: State>(peripheral: P) -> Self {
+        Stateful {
+            peripheral,
+            _state: std::marker::PhantomData,
+        }
+    }
+    
+    pub fn transition<NewS: State>(self) -> Stateful<P, NewS> 
+    where 
+        P: InState<NewS>,
+        P: ValidTransition<S, NewS>,
+    {
+        Stateful {
+            peripheral: self.peripheral,
+            _state: std::marker::PhantomData,
+        }
+    }
+    
+    pub fn get(&self) -> &P {
+        &self.peripheral
+    }
+    
+    pub fn expect<ExpectedS: State>(self) -> Self 
+    where 
+        S: std::cmp::PartialEq<ExpectedS>,
+        P: InState<ExpectedS>
+    {
+        self
+    }
+}
+
+#[macro_export]
+macro_rules! define_transition {
+    ($peripheral:ty, $from:ty, $to:ty) => {
+        impl ValidTransition<$from, $to> for $peripheral {}
+    };
+}
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Idle { Default}
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+
+pub enum Configured {Default}
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+
+pub enum Running {Default}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+
+pub enum Stop { Default}
+impl State for Idle {}
+impl State for Configured {}
+impl State for Running {}
+impl State for Stop {}
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+
+pub enum States {
+    Idle(Idle),
+    Configured(Configured),
+    Running(Running),
+    Stop(Stop),
+}
+
+impl State for States {}
+
+// actually implementing the peripheral
+pub struct I2CBus {
+    // pretend this is a real i2c bus
+}
+
+impl I2CBus {
+    fn new() -> Self {
+        I2CBus {}
+    }
+    
+    // Each method would like actually do stuff with the hardware
+    fn configure(&mut self, num: u32) {
+        println!("configured with number: {num}")
+    }
+    
+    fn start(&mut self) {
+        println!("started")
+    }
+    
+    fn stop(&mut self) {
+        println!("stopped")
+    }
+
+    fn idle(&mut self) {
+        println!("idling")
+    }
+}
+
+impl Peripheral for I2CBus {
+    type State = States;
+}
+
+// Define which states the I2C bus can be in
+impl InState<Stop> for I2CBus {}
+impl InState<Configured> for I2CBus {}
+impl InState<Running> for I2CBus {}
+impl InState<Idle> for I2CBus {}
+
+// valid state transitions
+define_transition!(I2CBus, Stop, Idle);
+define_transition!(I2CBus, Idle, Configured);
+define_transition!(I2CBus, Configured, Running);
+define_transition!(I2CBus, Running, Idle);
+define_transition!(I2CBus, Idle, Stop);
+
+impl Stateful<I2CBus, Idle> {
+    pub fn configure(mut self, number: u32) -> Stateful<I2CBus, Configured> {
+        self.peripheral.configure(number);
+        self.transition::<Configured>()
+    }
+
+    pub fn stop(mut self) -> Stateful<I2CBus, Stop> {
+        self.peripheral.stop();
+        self.transition::<Stop>()
+    }
+}
+
+impl Stateful<I2CBus, Configured> {
+    pub fn start(mut self) -> Stateful<I2CBus, Running> {
+        self.peripheral.start();
+        self.transition::<Running>()
+    } 
+}
+
+impl Stateful<I2CBus, Running> {
+    pub fn idle(mut self) -> Stateful<I2CBus, Idle> {
+        self.peripheral.idle();
+        self.transition::<Idle>()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bus = I2CBus::new();
+    let idle = Stateful::<I2CBus, Idle>::new::<Idle>(bus);
+    let configured = idle.configure(1000);
+    
+    let running = configured.start();
+
+    let idle = running.idle();
+    let test: Stateful<I2CBus, _>;
+    if true == true {
+        let stopped = idle.configure(123).start().idle().stop();
+        stopped.expect::<Stop>();
+        // stopped.start()
+    } else {
+        test = idle.configure(123).start().idle().stop();
+    }
+
+    // test.expect::<Running>();
 }
